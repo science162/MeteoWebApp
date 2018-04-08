5a52d4e093cf5920f0b85910e15a2941eb29dce1
Albert <albertomouaffo@gmail.com>
2018-04-08 21:02:44 +0200

    first comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MeteoService } from './meteo.service';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CurrentComponent } from './current/current.component';
+import { MeteoComponent } from './meteo/meteo.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { ResolveLocationService } from './resolve-location.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CurrentComponent,
+    MeteoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [MeteoService, ResolveLocationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
